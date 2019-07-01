fd7178704ef47a5b56128fdbcfb3f0d3b5239cb5
Robert Redd <raredd01@gmail.com>
2019-07-01 12:23:33 -0400

    bin1pow - add alpha calculation

--- R/desmon-extra.R
+++ R/desmon-extra.R
@@ -5,10 +5,12 @@
 
 #' Power for one-sample exact binomial tests
 #' 
-#' Determines the power for a one-sided, one-sample exact binomial test.
+#' Determines the power and significance level for a one-sided, one-sample
+#' exact binomial test.
 #' 
 #' @param n sample size
-#' @param pa probability of success under alternative hypothesis
+#' @param p0,pa probability of success under the null and alternative
+#' hypotheses, respectively
 #' @param r critical value
 #' 
 #' @examples
@@ -16,21 +18,21 @@
 #' pa <- 0.3
 #' 
 #' des <- desmon2:::bin1samp(p0, pa)
-#' 1 - des[['type2']]
+#' des[c('size', 'type2')]
 #' 
-#' bin1pow(des['n'], pa, des['r'] + 1L)
+#' bin1pow(des['n'], p0, pa, des['r'] + 1L)
 #' 
 #' @export
 
-bin1pow <- function(n, pa, r) {
+bin1pow <- function(n, p0, pa, r) {
   stopifnot(
     length(n) == 1L,
     length(pa) == 1L
   )
   
-  bin1pow_ <- function(n, pa, r) {
-    sum(dbinom(seq(r, n), n, pa))
+  bin1pow_ <- function(n, p, r) {
+    sum(dbinom(seq(r, n), n, p))
   }
   
-  Vectorize(bin1pow_, 'r', TRUE, FALSE)(n, pa, r)
+  c(type1 = bin1pow_(n, p0, r), type2 = 1 - bin1pow_(n, pa, r))
 }
